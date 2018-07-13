ee75fcdcb8ec8e6a25f099b9ddfe982cafd7dc52
JackRMartin <36291367+JackRMartin@users.noreply.github.com>
2018-07-13 14:57:21 -0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,7 +2,7 @@
 import tweepy
 from tweepy import OAuthHandler
 import credentials
- 
+
 consumer_key = credentials.consumer_key
 consumer_secret = credentials.consumer_secret
 access_token = credentials.access_token
@@ -11,7 +11,26 @@ access_secret = credentials.access_secret
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
  
+ #open file and get twitter name to use
+twitter_name_file = open("url.txt","r")
+twitter_name = twitter_name_file.readline();
+
 api = tweepy.API(auth)
+user = api.get_user(twitter_name)
+
+print (user.screen_name)
+print (user.followers_count)
+
+statuses = api.user_timeline('thundawolf')
+
+
+outputfile = open("output.txt", "a")
+for status in statuses:
+	this_status = status.text
+	print (this_status)
+	outputfile.write(this_status)
+
+
 
 
 
