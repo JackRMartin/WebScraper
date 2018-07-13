2aa72acee3edcd266187493eb879f162ede29d3d
JackRMartin <36291367+JackRMartin@users.noreply.github.com>
2018-07-13 11:08:39 -0700

    commit

--- Source.cpp
+++ Source.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <fstream>
 
+//python script must be located in the same directory as the c++ code that calls the script
+
 int main()
 {
-	//python script must be located in the same directory as the c++ code that calls the script
+	/// CODE HERE IS FOR LINKING PYTHON AND C++ 
+	/// DO NOT EDIT
+	/// -------------------------------------------
+	system("chcp 65001");
+	system("CLS");
+	std::cout << "Running Script..." << std::endl;
 	system("python scraper.py");
+	/// -------------------------------------------
+
+
 	return 0;
 }
