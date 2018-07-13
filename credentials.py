bfa4aee00858898bea196430c95c9932a1c5911b
JackRMartin <36291367+JackRMartin@users.noreply.github.com>
2018-07-13 11:33:55 -0700

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,4 @@
+consumer_key = 	"rlhwLxoZXhcHkUdpGkxgC6l4Q"
+consumer_secret = "sDIZRQED8IulvaqHF38uVe2okJ5MhnLp2IXfat0aO2CG7pO5S9"
+access_token = 	"3241320433-ncFN8xXUBEuFa2wt0RVwKxL4GrdGY6BksZul4EM"
+access_secret = "QKVVVwZ9gtUQ2bUn0MVZNsEuhsO3mXSu6SbUvgwHOp3N1"
